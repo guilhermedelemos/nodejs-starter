5221cebbf07b8d158b162446abf1ace5c28ca3d7
Guilherme de Lemos <guilherme.eti@gmail.com>
2019-03-17 17:37:40 -0300

    Fix plugins

--- src/plugins.js
+++ src/plugins.js
@@ -1,25 +1,20 @@
 const _ = require('lodash');
 const plugins = [];
 
-const productionPlugins = [];
-
-const developmentPlugins = [{
+if (process.env.NODE_ENV !== 'production') {
+    plugins.push({
         plugin: require('blipp'),
         options: {
             showAuth: true
         }
-    },
-    /*{
+    });
+    /*plugins.push({
         plugin: require('hapi-pino'),
         options: {
             prettyPrint: true,
             logEvents: ['response', 'onPostStart']
         }
-    }*/
-];
-
-plugins.push(productionPlugins);
-if (process.env.NODE_ENV !== 'production') {
-    plugins.push(developmentPlugins);
+    });*/
 }
-module.exports = _.flattenDeep(plugins);
+
+module.exports = plugins;
